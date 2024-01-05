a55181c4aecb7b16fe718e1e7429e1c55bcc380f
i-xi-dev <naoyuki.isono@gmail.com>
2024-01-05 21:52:00 +0900

    5.4.0

--- tests/uint8.test.ts
+++ tests/uint8.test.ts
@@ -313,3 +313,49 @@ Deno.test("SafeInteger.toString(any)", () => {
     "source",
   );
 });
+
+Deno.test("Uint8.rotateLeft(number, number)", () => {
+  assertStrictEquals(Uint8.rotateLeft(0b10000000, 0), 0b10000000);
+  assertStrictEquals(Uint8.rotateLeft(0b10000000, 1), 0b00000001);
+  assertStrictEquals(Uint8.rotateLeft(0b10000000, 2), 0b00000010);
+  assertStrictEquals(Uint8.rotateLeft(0b10000000, 3), 0b00000100);
+  assertStrictEquals(Uint8.rotateLeft(0b10000000, 4), 0b00001000);
+  assertStrictEquals(Uint8.rotateLeft(0b10000000, 5), 0b00010000);
+  assertStrictEquals(Uint8.rotateLeft(0b10000000, 6), 0b00100000);
+  assertStrictEquals(Uint8.rotateLeft(0b10000000, 7), 0b01000000);
+  assertStrictEquals(Uint8.rotateLeft(0b10000000, 8), 0b10000000);
+
+  assertStrictEquals(Uint8.rotateLeft(0b01111111, 0), 0b01111111);
+  assertStrictEquals(Uint8.rotateLeft(0b01111111, 1), 0b11111110);
+  assertStrictEquals(Uint8.rotateLeft(0b01111111, 2), 0b11111101);
+  assertStrictEquals(Uint8.rotateLeft(0b01111111, 3), 0b11111011);
+  assertStrictEquals(Uint8.rotateLeft(0b01111111, 4), 0b11110111);
+  assertStrictEquals(Uint8.rotateLeft(0b01111111, 5), 0b11101111);
+  assertStrictEquals(Uint8.rotateLeft(0b01111111, 6), 0b11011111);
+  assertStrictEquals(Uint8.rotateLeft(0b01111111, 7), 0b10111111);
+  assertStrictEquals(Uint8.rotateLeft(0b01111111, 8), 0b01111111);
+
+  assertStrictEquals(Uint8.rotateLeft(0b11111111, 1), 0b11111111);
+
+  assertThrows(
+    () => {
+      Uint8.rotateLeft(256 as Uint8, 1);
+    },
+    TypeError,
+    "source",
+  );
+  assertThrows(
+    () => {
+      Uint8.rotateLeft(-1 as Uint8, 1);
+    },
+    TypeError,
+    "source",
+  );
+  assertThrows(
+    () => {
+      Uint8.rotateLeft(255, 9 as 8);
+    },
+    TypeError,
+    "amount",
+  );
+});
